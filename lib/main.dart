a9354e2d4b8511ae08d4561f8021f23ff0828361
piyanut <chaiyanut23@gmail.com>
2024-06-30 18:34:42 +0700

    ทดสอบ

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(
-        title: 'Learn app for you',
+        title: 'LearnAppForYou',
       ),
     );
   }
