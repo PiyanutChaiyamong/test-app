dfd85e1108c2c5ebc805eb008ecc3f52b062ba0b
piyanut <chaiyanut23@gmail.com>
2024-07-06 14:25:39 +0700

    ทดสอบ10

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: ' J App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(
-        title: 'LearnApp JGJGJGJG ',
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+  var app = MaterialApp(
+    title: "My App",
+    home: Scaffold(
+        appBar: AppBar(
+          title: const Text("NongGmail"),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        child: const Icon(Icons.add_circle_outline),
-      ),
-    );
-  }
+        body: Column(
+          children: [
+            Container(
+              decoration:
+                  const BoxDecoration(color: Color.fromARGB(255, 128, 238, 18)),
+              height: 100, //กำหนดความสูง
+            ),
+            Container(
+              decoration:
+                  const BoxDecoration(color: Color.fromARGB(242, 206, 225, 36)),
+              height: 580, //กำหนดความสูง
+            ),
+            Container(
+              decoration:
+                  const BoxDecoration(color: Color.fromARGB(255, 35, 150, 156)),
+              height: 100, //กำหนดความสูง
+            )
+          ],
+        )),
+  );
+  runApp(app);
 }
