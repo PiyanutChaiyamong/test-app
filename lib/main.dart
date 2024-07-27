4abc9015c95259396a94bc5bf6f89f7e3ecea0ce
piyanut <chaiyanut23@gmail.com>
2024-07-27 10:35:22 +0700

    ทดสอบ11

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,54 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  var app = MaterialApp(
-    title: "My App",
-    home: Scaffold(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My Title",
+      home: Scaffold(
         appBar: AppBar(
-          title: const Text("NongGmail"),
+          title: const Text("My App"),
+          backgroundColor: Colors.green,
+          centerTitle: true,
         ),
-        body: Column(
-          children: [
-            Container(
-              decoration:
-                  const BoxDecoration(color: Color.fromARGB(212, 237, 11, 11)),
-              height: 100, //กำหนดความสูง
-            ),
-            Container(
-              decoration:
-                  const BoxDecoration(color: Color.fromARGB(255, 255, 255, 255)),
-              height: 630, //กำหนดความสูง
-            ),
-            Container(
-              decoration:
-                  const BoxDecoration(color: Color.fromARGB(255, 35, 150, 156)),
-              height: 50, //กำหนดความสูง
-            )
-          ],
-        )),
-  );
-  runApp(app);
+        body: Container(
+          margin: const EdgeInsets.fromLTRB(
+              0, 5, 0, 600), // กำหนดระยะห่างขอบนอก ให้ container
+          padding: const EdgeInsets.fromLTRB(
+              20, 5, 20, 20), // กำหนดระยะห่างขอบด้านใน
+          decoration: const BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+          ),
+          child: Column(
+            children: [
+              const TextField(
+                decoration:
+                    InputDecoration(labelText: 'Enter your notes here!'),
+                style: TextStyle(fontSize: 40, color: Colors.redAccent),
+                textAlign: TextAlign.center, // ให้ text อยู่ตรงกลาง
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.purple, // สีพื้นหลังของปุ่ม
+                  foregroundColor: Colors.white, // สีข้อความของปุ่ม
+                ),
+                child: const Text(
+                  "Click me",
+                  style: TextStyle(fontSize: 30),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
